9ac1b8218a3a0bc9fe593edea6a85c4bc1b43019
Ph4ra0hX <ph4ra0hx@gmail.com>
2022-12-02 19:22:03 -0300

    ajuste nos precos

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 
 const port = 3000 || process.env.PORT;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.json("Hello World!");
+  res.sendFile(path.join(__dirname + "/views/aviso.html"));
 });
 
 app.listen(port || process.env.PORT, () => {
