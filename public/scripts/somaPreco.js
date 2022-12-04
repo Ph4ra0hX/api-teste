b71000834b8736ba87a6735f1f8ddecf0926d621
Ph4ra0hX <ph4ra0hx@gmail.com>
2022-12-04 18:55:28 -0300

    ajuste nos precos

--- public/scripts/somaPreco.js
+++ public/scripts/somaPreco.js
@@ -2,7 +2,7 @@ function UpdateCost() {
   var sum = 0;
   var gn, elem;
   var gn, elem;
-  for (i = 1; i <= 25; i++) {
+  for (i = 1; i <= 24; i++) {
     gn = "game" + i;
     elem = document.getElementById(gn);
     if (elem.checked == true) {
